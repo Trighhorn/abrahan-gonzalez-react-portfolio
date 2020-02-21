de4503221403c98c2ca64f2ee514c7237a239a24
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-21 09:42:13 -0700

    Added portflio items and added render for portfoio item in portfolio container/cuntructor added to container

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 
 
+import PortfolioItem from './portfolio-item'
 
 export default class PortfolioContainer extends Component {
+  constructor() {
+    super()
+    console.log('container has rendered')
+  }
   render() {
     return (
       <div>
         <h2>
           Portfolio Items go here...Updated
         </h2>
+
+        <PortfolioItem />
       </div>
     )
   }
