005831c765fc4a1d4dda74efa547524e102afb7c
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-24 14:33:05 -0700

    added temporary filter function

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,25 +9,29 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my Portfolio",
       data: [
-        {title: "Quip"}, 
-        {title: "Eventbrite"},
-        {title: "placeholder 3"},
-        {title: "placeholder 4"}
+        {title: "Quip", catrgory: "eCommerce"}, 
+        {title: "Eventbrite", catrgory: "Scheduling"},
+        {title: "placeholder 3", catrgory: "Enterprise"},
+        {title: "placeholder 4", catrgory: "eCommerce"}
       ]
     }
+    this.handleFilter = this.handleFilter.bind(this)
 
-    this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
+  }
+
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.catrgory === filter
+      })
+    })
   }
 
   portfolioItems () {
     return this.state.data.map(item => <PortfolioItem title={item.title} url='placeholder url' />)
   }
 
-  handlePageTitleUpdate () {
-    this.setState({
-      pageTitle: "AHHHHHHHHHHHHHHHHHHHH Im trapped in a webbage"
-    })
-  }
   render() {
     return (
       <div>
@@ -38,10 +42,14 @@ export default class PortfolioContainer extends Component {
         <h2>
           Portfolio Items go here...Updated
         </h2>
-        {this.portfolioItems()}
 
-        <hr/>
-        <button onClick={this.handlePageTitleUpdate}>Change title</button>
+        <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+        <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+        <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+
+
+
+        {this.portfolioItems()}
       </div>
     )
   }
