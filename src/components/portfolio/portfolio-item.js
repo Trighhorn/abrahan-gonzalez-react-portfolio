bf3ba96904f8f42df3337154d02e9d186bb48de3
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-26 14:24:13 -0700

    added inline style to add imaged to the background and added styling in the scss for the images

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -6,10 +6,17 @@ export default function portfolioItem(props) {
   const { id, description, thumb_image_url, logo } = props.item
   return (
     <div className="portfolio-item-wrapper">
-      <img src={thumb_image_url} />
+      <div 
+        className="portfolio-img-background" 
+        style={{
+          backgroundImage: `url(${thumb_image_url})`
+        }}
+      />
+
+
       <img src={logo} />
       <div>{description}</div>
-      <Link to={`/portfolio/${id}}`}>Link</Link>
+      <Link to={`/portfolio/${id}`}>Link</Link>
     </div>
   )
 }
