72230683027f3874ca279e2a9982f480d625bcb6
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-27 16:22:17 -0700

    added the functionality to the login state

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -38,11 +38,12 @@ handleSubmit(event) {
   { withCredentials: true }
   ).then(response => {
     if (response.data.status === "created") {
-      console.log("You can come in...");
+      this.props.handleSuccessfulAuth();
     } else {
       this.setState({
         errorText: "Wrong email or password"
       });
+      this.props.handleUnSuccessfulAuth();
     }
   })
   .catch(error => {
