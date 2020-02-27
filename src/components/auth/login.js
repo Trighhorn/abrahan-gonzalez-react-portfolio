d1ebedb5c2a285df251dc7ea4de8e16d3dc02d4e
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-27 13:28:08 -0700

    fleshed out form to update when typing in to set state for use later

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+    email: '',
+    password: ''
+  }
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+// no bind method
+// handleChange = (event) => {
+//   console.log("Handle change", event);
+// }
+
+handleChange(event) {
+  this.setState({
+    [event.target.name]: event.target.value
+  });
+}
+
+handleSubmit(event) {
+  console.log("Handle submit", event);
+}
+
   render() {
     return (
         <div>
             <h1>Login to access your dashboard</h1>
-            <form>
-              <input type='text' />
-              <input type='password' />
+            <form onSubmit={this.handleSubmit}>
+              <input 
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <input 
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.handleChange} 
+              />
+              <div>
+                <button type="submit">Login</button>
+              </div>
             </form>
         </div>
     );
