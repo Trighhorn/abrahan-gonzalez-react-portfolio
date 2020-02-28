baf8af0dc8b3feb7babddb278363f40538688ae7
Abrahan Gonzalez <trighhorn@gmail.com>
2020-02-28 16:42:31 -0700

    added api plugins for portfolio manager

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://abrahangonzalez.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
   render() {
     return (
       <div>
