f41cb298d8429cf0d5b0b248202828c1c8001dc0
Abrahan Gonzalez <trighhorn@gmail.com>
2020-03-03 14:54:07 -0700

    added font awesome icon for signout with styling

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import axios from 'axios';
 import { withRouter } from 'react-router';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavigationContainer = props => {
   const dynamicLink = (route, linkText) => {
@@ -58,8 +59,11 @@ const NavigationContainer = props => {
           : null}
       </div>
       <div className="right-side">
-        ABRAHAN GONZALEZ
-        {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null}
+        ABRAHAN GONZALEZ 
+        {props.loggedInStatus === "LOGGED_IN" ? (
+        <a onClick={handleSignOut}>
+            <FontAwesomeIcon icon="sign-out-alt" />
+        </a>) : null}
       </div>
     </div>
   );
