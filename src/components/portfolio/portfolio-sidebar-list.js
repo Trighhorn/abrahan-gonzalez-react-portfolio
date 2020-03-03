3a8d150b82d5745295f14d77063bdf0e637ecc23
Abrahan Gonzalez <trighhorn@gmail.com>
2020-03-03 14:40:46 -0700

    added styling for portfolio sidebar

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const PortfolioSidebarList = (props) => {
+const PortfolioSidebarList = props => {
   const portfolioList = props.data.map(portfolioItem => {
-    return ( 
+    return (
       <div className="portfoli-item-thumb" key={portfolioItem.id}>
         <div className="portfoli-thumb-img">
           <img src={portfolioItem.thumb_image_url} />
         </div>
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        <a onClick={() => props.handleDeleteClick(portfolioItem)}>
-          <FontAwesomeIcon icon="trash" />
-        </a>
-      </div>
-    )
-  })
-  return (
-    <div className="portfolio-sidebar-list-wrapper">
-      {portfolioList}
-    </div>
-  )
-}
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
 
-export default PortfolioSidebarList
+          <a
+            className="delete-icon"
+            onClick={() => props.handleDeleteClick(portfolioItem)}
+          >
+            <FontAwesomeIcon icon="trash" />
+          </a>
+        </div>
+      </div>
+    );
+  });
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+};
+
+export default PortfolioSidebarList;
