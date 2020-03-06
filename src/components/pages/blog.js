9195878e4ba5244a5f4fcf7c0fc6f0a78312710f
Abrahan Gonzalez <trighhorn@gmail.com>
2020-03-06 11:45:28 -0700

    added styles to the main blog page

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
-import BlogItem from '../blog/blog-item';
+import BlogItem from "../blog/blog-item";
 
 export default class Blog extends Component {
   constructor() {
@@ -10,41 +10,38 @@ export default class Blog extends Component {
 
     this.state = {
       blogItems: []
-    }
+    };
 
-    this.getBlogItems = this.getBlogItems.bind(this)
+    this.getBlogItems = this.getBlogItems.bind(this);
   }
 
   getBlogItems() {
-    axios.get
-    ("https://abrahangonzalez.devcamp.space/portfolio/portfolio_blogs", 
-    { withCredentials: true }
-    ).then(response => {
-      this.setState({
-        blogItems: response.data.portfolio_blogs
+    axios
+      .get("https://abrahangonzalez.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
       })
-    }).catch( error => {
-      console.log("getBlogItems error", error)
-    });
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
   }
 
   componentDidMount() {
-    this.getBlogItems()
+    this.getBlogItems();
   }
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem}/>
-    })
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+
     return (
-      <div>
-        <h2>Blog</h2>
-        <div>
-          {blogRecords}
-        </div>
-        <div>
-          <Link to="/about-me">Read more about me!</Link>
-        </div>
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
       </div>
     );
   }
