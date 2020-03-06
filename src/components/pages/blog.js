9c3dc6c3a47c99ff0d2158043b815c0b9c8ea872
Abrahan Gonzalez <trighhorn@gmail.com>
2020-03-06 12:13:18 -0700

    added states to update on trigger that need to be implemented

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -9,7 +9,9 @@ export default class Blog extends Component {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
@@ -28,13 +30,17 @@ export default class Blog extends Component {
   }
 
   getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    })
     axios
       .get("https://abrahangonzalez.devcamp.space/portfolio/portfolio_blogs", {
         withCredentials: true
       })
       .then(response => {
         this.setState({
-          blogItems: response.data.portfolio_blogs
+          blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records
         });
       })
       .catch(error => {
