44882c6b57b21189e92e3ca251309a9d0ea56d14
Abrahan Gonzalez <trighhorn@gmail.com>
2020-03-06 11:09:23 -0700

    added call to api to get blog data

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
   }
+
+  getBlogItem() {
+    axios.get(
+      `https://abrahangonzalez.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+    ).then(response => {
+      console.log("response", response)
+    }).catch(error => {
+      console.log("geBlogItem Error", error)
+    })  
+  }
+
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
   render() {
     return (
       <div>
