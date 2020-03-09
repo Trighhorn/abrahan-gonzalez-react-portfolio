d62dfe9c438bfa2f1190f27d4e756853ce71107b
Abrahan Gonzalez <trighhorn@gmail.com>
2020-03-09 16:49:29 -0600

    fixed a typo

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -8,11 +8,17 @@ export default class BlogForm extends Component {
     super(props);
     this.state = {
       title: "",
-      blog_status: ""
+      blog_status: "",
+      content: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+  }
+
+  handleRichTextEditorChange(content) {
+    this.setState({ content });
   }
 
   buildForm() {
@@ -20,6 +26,7 @@ export default class BlogForm extends Component {
 
     formData.append("portfolio_blog[title]", this.state.title);
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+    formData.append("portfolio_blog[content]", this.state.content);
 
     return formData;
   }
@@ -83,7 +90,9 @@ export default class BlogForm extends Component {
           </select> */}
         </div>
         <div className="one-column">
-          <RichTextEditor />
+          <RichTextEditor
+            handleRichtexteditorChange={handleRichtexteditorChange}
+          />
         </div>
         <input
           className="submit-btn"
